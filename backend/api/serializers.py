4051364708e46b48363d757d0f405e79a7eee14c
Belova Olga <obelova2003@yandex.ru>
2024-05-25 12:18:57 +0300

    serializers.py

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,15 +1,23 @@
+from rest_framework.fields import SerializerMethodField
 from shop.models import Organization, Shop
 
 from rest_framework import serializers
 
 
-class OrganizationsSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Organization
-        fields = '__all__'
-
-
 class ShopsSerializer(serializers.ModelSerializer):
     class Meta:
         model = Shop
-        fields = '__all__'
+        fields = ('name', 'description', 'address', 'index')
+
+
+class OrganizationsSerializer(serializers.ModelSerializer):
+    shops = SerializerMethodField()
+
+    class Meta:
+        model = Organization
+        fields = ('name', 'description', 'shops')
+
+    def get_shops(self, organization):
+        shops = Shop.objects.filter(organization_id=organization.id,
+                                    is_deleted=False)
+        return ShopsSerializer(shops, many=True).data

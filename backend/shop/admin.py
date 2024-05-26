672ea2836c5fa5c83bd3c231df4897219b53f4d0
Belova Olga <obelova2003@yandex.ru>
2024-05-26 14:10:06 +0300

    inlines

--- backend/shop/admin.py
+++ backend/shop/admin.py
@@ -2,5 +2,14 @@ from django.contrib import admin
 
 from .models import Organization, Shop
 
-admin.site.register(Organization)
 admin.site.register(Shop)
+
+
+class OrganizationShopInline(admin.TabularInline):
+    model = Shop
+    extra = 1
+
+
+@admin.register(Organization)
+class OrganizationAdmin(admin.ModelAdmin):
+    inlines = [OrganizationShopInline, ]
--- backend/shop/models.py
+++ backend/shop/models.py
@@ -1,9 +1,6 @@
 from django.db import models
 
 
-# Create your models here.
-
-
 class Organization(models.Model):
     name = models.CharField(max_length=150,
                             verbose_name='Название организации')
